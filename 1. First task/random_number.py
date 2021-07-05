003c174040e4dcf5a7bb1386a35e3e893d1f9a7c
triplea <triplea@jb-host.net>
2021-07-05 16:28:46 +0200

    first task

---
+++ 1. First task/random_number.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+	
+# generate random integer values
+from random import randint
+
+# generate some integers
+for _ in range(1,10):
+	value = randint(1, 10)
+	print(value)
